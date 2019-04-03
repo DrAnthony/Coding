f037057d0e959c9346b61f4ef1a15de812a72c3d
DrAnthony <dikwang886@gmail.com>
2019-04-03 10:59:35 +0800

    网络软件系统设计技术-后端-second

--- src/priv/wd/DAO/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package priv.wd.DAO;
-
-import priv.wd.Entity.UserEntity;
-
-public interface UserDAO {
-	public void addUser(UserEntity user);
-	public UserEntity getUser(String column,String str);
-}
--- src/priv/wd/Service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package priv.wd.Service;
-
-import priv.wd.Entity.EmailEntity;
-
-public interface EmailService {
-	public void sendCode(String eamil);
-	public boolean checkCode(EmailEntity email);
-	public void deleteEmail(EmailEntity email);
-}
